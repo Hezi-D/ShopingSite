e2921cef13a446ea626bd02f04f72d910a0b01c8
Hezi-D <dddavid1984@gmail.com>
2019-03-19 00:06:43 +0200

    second commit

--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,13 +1,57 @@
 import React from "react";
 import { connect } from "react-redux";
 
-export const cartItemsWithQusntities = cartItem => {
-  return cartItem.reduce((acc, item) => {
-    const filterItem = acc.filter(item2 => item2.id === item.id)[0];
-    filterItem !== undefined
-      ? filterItem.quantity++
-      : acc.push({ ...item, quantity: 1 });
+function Cart(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Quantity</th>
+          <th />
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {props.cart.map(item => (
+          <tr>
+            <td>item.name</td>
+            <td>item.quantity</td>
+            <td>
+              <button onClick={() => props.addToCart(item)}>+</button>
+            </td>
+            <td>
+              <button onClick={() => props.removeFromCart(item)}>-</button>
+            </td>
+            <td>
+              <button onClick={() => props.removeAllFromCart(item)}>
+                Remove all from cart
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
-    return acc;
-  }, []);
-};
+function mapStateToProps(state) {
+  return {
+    cart: state.cart
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToCart: item => {
+      dispatch({ type: "ADD", payload: item });
+    },
+    removeFromCart: item => {
+      dispatch({ type: "REMOVE", payload: item });
+    },
+    removeAllFromCart: item => {
+      dispatch({ type: "REMOVE_ALL", payload: item });
+    }
+  };
+}
+export default connect(mapStateToProps)(Cart);
